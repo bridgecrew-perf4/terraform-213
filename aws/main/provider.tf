2b613bcf4e329e66560e66b58092dae29d014186
nishantkj911 <38178615+nishantkj911@users.noreply.github.com>
2021-01-13 20:59:50 +0530

    Added networking components - VPC, subnet, gateway, etc. Added Automation scripts

--- aws/main/provider.tf
+++ aws/main/provider.tf
@@ -6,8 +6,18 @@ terraform {
 
     }
   }
+
+  backend "remote" {
+    organization = "nkjcorp"
+
+    workspaces {
+      name = "aws"
+    }
+  }
 }
 
 provider "aws" {
   region = "ap-south-1" // Mumbai region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
---
+++ aws/main/variables.tf
@@ -0,0 +1,22 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "access_key" {
+  description = "The AWS access key of the gen account"
+}
+
+variable "secret_key" {
+  description = "The secret of the provided access key"
+}
+
+variable "image_id" {
+  default = "ami-04b1ddd35fd71475a" // Amazon Linux 2 AMI 64-bit x86
+  description = "The ID of the image being used to deploy the instance"
+}
+
+variable "instance_count" {
+  default = 2
+  type = number
+  description = "The number of instances to create"
+}
