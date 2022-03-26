658fd7c243636228de4394353b876062afec2824
nishantkj911 <38178615+nishantkj911@users.noreply.github.com>
2022-03-26 17:04:13 +0530

    Add ansible automation playbooks for a master-worker cluster creation

--- aws/main/automation/scripts/initial_tasks.sh
+++ aws/main/automation/scripts/initial_tasks.sh
@@ -1,3 +1,6 @@
+# Note: commands mentioned here are to be run manually. It's not for automation purposes.
+#       It is currently configured to NOT use controller node and run operations locally.
+
 # Register with Redhat for RHEL VMs
 subscription-manager register --username nishantkj911
 subscription-manager attach --auto
@@ -8,5 +11,5 @@ dnf -y update
 # install ansible
 yum install -y ansible
 
-ansible-playbook playbooks/initial-tasks.yml
-ansible-playbook playbook/install-master.yml
+#ansible-playbook playbooks/initial-tasks.yml
+ansible-playbook playbooks/new_kubernetes_cluster.yml         # Doesn't work on Windows systems.
